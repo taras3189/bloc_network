5fde145aff8fb0d986e9564c21bd0391a90cae32
Taras Bobriienko <taras3189@gmail.com>
2023-04-05 14:29:29 +0300

    3 commit

---
+++ lib/services/user_api_provider.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+import '../model/user.dart';
+import 'package:http/http.dart' as http;
+
+class UserProvider {
+
+  //https:jsonplaceholder.typicode.com/users
+
+  Future<List<User>> getUsers() async {
+    final response = await http.get(
+        Uri.parse('https:jsonplaceholder.typicode.com/users'));
+
+    if (response.statusCode == 200) {
+      final List<dynamic> userJson = json.decode(response.body);
+      return userJson.map((json) => User.fromJson(json)).toList();
+    } else {
+      throw Exception('Error fetching users');
+    }
+  }
+}
--- lib/widgets/user_list.dart
+++ lib/widgets/user_list.dart
@@ -9,10 +9,30 @@ class UserList extends StatelessWidget {
       itemCount: 20,
       itemBuilder: (context, index) => Container(
         color: index % 2 == 0 ? Colors.white : Colors.blue[50],
-        child: const ListTile(
-          leading: Text(
+        child: ListTile(
+          leading: const Text(
             'ID 1',
             style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          title: Column(
+            children: [
+              const Text(
+                'My Name',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Column(
+                children: const [
+                  Text(
+                    'Email: test@test.net',
+                    style: TextStyle(fontStyle: FontStyle.italic),
+                  ),
+                  Text(
+                    'Phone 1234564789',
+                    style: TextStyle(fontStyle: FontStyle.italic),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
